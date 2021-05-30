fbeb3c1726bbce765e6a7471f26461a51e81157b
therishabhdixit <69054923+therishabhdixit@users.noreply.github.com>
2021-05-30 17:54:57 +0530

    animation

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext } from 'react'
 import ContactItem from './ContactItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import ContactContext from '../../context/contact/conatactContext'
 
 const Contacts = () => {
@@ -12,10 +13,19 @@ const Contacts = () => {
     }
     return (
         <Fragment>
-            {filtered != null ? filtered.map(contact => (<ContactItem key={contact.id}
-                contact={contact} />)) : contacts.map(contact => (
-                    <ContactItem key={contact.id} contact={contact} />
+            <TransitionGroup>
+
+                {filtered != null ? filtered.map(contact => (
+                    <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                        <ContactItem
+                            contact={contact} />
+                    </CSSTransition>
+                )) : contacts.map(contact => (
+                    <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                        <ContactItem contact={contact} />
+                    </CSSTransition>
                 ))}
+            </TransitionGroup>
         </Fragment>
     )
 }
