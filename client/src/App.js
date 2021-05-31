3304d63e6a8a056b1f903719168f815d66bb7a42
therishabhdixit <69054923+therishabhdixit@users.noreply.github.com>
2021-05-31 16:04:23 +0530

    loginRegister

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,32 @@ import './App.css';
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from "./components/auth/Login";
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
